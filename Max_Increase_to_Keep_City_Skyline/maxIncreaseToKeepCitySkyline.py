6579c94167180c1f5b142e3183cb1fb6acfcc5c5
TommyRedblue <zbrucyp@gmail.com>
2018-09-23 10:41:18 -0400

    Update maxIncreaseToKeepCitySkyline.py
    Removing unnecessary lines.
    Making variable names more clear.

--- Max_Increase_to_Keep_City_Skyline/maxIncreaseToKeepCitySkyline.py
+++ Max_Increase_to_Keep_City_Skyline/maxIncreaseToKeepCitySkyline.py
@@ -6,7 +6,7 @@ class Solution:
         """
         verticalMaxes = self.findVerticalMaxes(grid)
         horizontalMaxes = self.findHorizontalMaxes(grid)
-        
+
         maxIncrease = 0
         for x, row in enumerate(grid):
             for y, column in enumerate(grid[0]):
@@ -33,18 +33,19 @@ class Solution:
         
     def findHorizontalMaxes(self, grid):
         
+        column = 0
         max_list = []
-        currColumn = 0
         
         for outRow in grid:
             currMax = 0
             currRow = 0
             for inRow in grid:
-                num = grid[currRow][currColumn]
+                num = grid[currRow][column]
                 if num > currMax:
                     currMax = num
                 currRow = currRow + 1
             max_list.append(currMax)
-            currColumn = currColumn + 1
-
+            column = column + 1
+    
+        
         return max_list
