5cb68cd9227294ef5a104e156907da4daab3cb0a
TommyRedblue <zbrucyp@gmail.com>
2018-10-07 17:39:38 -0400

    Create searchBinarySearchTree.py
    https://leetcode.com/problems/search-in-a-binary-search-tree/description/

---
+++ Search_in_a_Binary_Search_Tree/searchBinarySearchTree.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        
+        if root is not None:
+            if root.val == val:
+                return root
+            else:
+                return self.searchTree(root, val)
+        else:
+            return None
+        
+        
+    def searchTree(self, node, searchVal):
+        
+        nodeVal = None
+        
+        if node is not None:
+            nodeVal = node.val  
+        
+        if node is None or int(nodeVal) == int(searchVal):
+            return node
+        
+        if searchVal < nodeVal:
+            return self.searchTree(node.left, searchVal)
+            
+        if searchVal > nodeVal:
+            return self.searchTree(node.right, searchVal)
